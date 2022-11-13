6252b83013af732add01a1334f4354c1b091d192
Leandro Pereira <leandroppmarques@gmail.com>
2022-11-13 16:26:07 -0300

    Exercício de escopo

--- escopo/script.js
+++ escopo/script.js
@@ -1,28 +1,43 @@
 // Por qual motivo o código abaixo retorna com erros?
+
+/*
 {
   var cor = 'preto';
   const marca = 'Fiat';
   let portas = 4;
 }
 console.log(var, marca, portas);
+*/
+
+//Porque foi declarado errado a variável de cor.
+//Porque a variável da const e do let estão fora do bloco.
+
+{
+  var cor = 'preto';
+  const marca = 'Fiat';
+  let portas = 4;
+
+  console.log(cor, marca, portas);
+}
+
 
 // Como corrigir o erro abaixo?
+const dois = 2;
 function somarDois(x) {
-  const dois = 2;
   return x + dois;
 }
 function dividirDois(x) {
-  return x + dois;
+  return x / dois;
 }
-somarDois(4);
-dividirDois(6);
+console.log (somarDois(4));
+console.log (dividirDois(6));
 
 // O que fazer para total retornar 500?
-var numero = 50;
+const numero = 50;
 
-for(var numero = 0; numero < 10; numero++) {
+for(let numero = 0; numero < 10; numero++) {
   console.log(numero);
 }
 
-const total = 10 * numero;
+var total = 10 * numero;
 console.log(total);
